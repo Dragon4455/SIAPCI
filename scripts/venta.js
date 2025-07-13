8209bfea901976d9f9077d53b18c99ce62202b00
SlimeDragon <128077927+Dragon4455@users.noreply.github.com>
2025-07-13 05:10:39 +0000

    Añadir función de buscar cliente y registrar cliente

--- scripts/venta.js
+++ scripts/venta.js
@@ -5,18 +5,18 @@ let BDCliente = [{
     rif: "31325616",
 },
 {
-    nombre: "Juan",
-    direccion: "San Esteban",
+    nombre: "Elías",
+    direccion: "Vistamar",
     tlf: "12345",
-    rif: "0123456",
+    rif: "31779388",
 }];
 let BDProducto = [{
         nombre: "Pega",
-        disponible:1,
+        disponible: 1,
         codigo: "12345",
         precio: 20,
 }];
-
+let clienteActual = "";
 
 // let cliente = {
 //     nombre: "Neomar",
@@ -30,39 +30,77 @@ let producto ={
         codigo: "12345",
         precio: 20,
 }
-function añadirProducto(nombre, cantidad,codigo,precio){
-   BDProducto.push({
-        nombre: this.nombre,
-        cantidad :this.cantidad,
-        codigo: this.codigo,
-        precio:this.precio,
-    })
+function añadirCliente(nombre, direccion,rif,tlf){
+   BDCliente.push({
+        nombre,
+        direccion,
+        rif,
+        tlf,
+    });
+    alert("Registro exitoso")
+    console.log(BDCliente)
 }
 
-function buscarCliente(){
+function buscarCliente(busqueda){
     
     let cliente = BDCliente.filter(item => $inputRif.value === item.rif);
+    
     if(cliente.length){
+        if(busqueda){
+            return true;
+        }
         $inputNomCli.value = cliente[0].nombre;
         $inputDir.value = cliente[0].direccion;
         $inputTel.value = cliente[0].tlf;
+        clienteActual = cliente[0]
         
-    } else{
-        alert("No encontrado")
+    } else{ 
+        if(busqueda){
+            return false
+        }
+        alert("No encontrado");
     }
 }
+function registrarCliente(){
+    if(!buscarCliente(true)){
+        console.log("Hola");
+        let nombre = $inputNomCli.value,
+            direccion = $inputDir.value,
+            rif = $inputRif.value,
+            tlf = $inputTel.value;
+            if(nombre && direccion && rif && tlf){
+                console.log(nombre)
+                añadirCliente(nombre,direccion,rif,tlf);
+                buscarCliente();
+            } else{
+                alert("Debes Ingresar toda la información")
+            }
+    } else{
+        alert("El usuario ingresado ya existe")
+    }
+}
+
+
+//Eventos de los botones
 
 let $inputNomCli = document.querySelector("#nombreCliente"),
     $inputRif = document.querySelector("#rifCliente"),
     $inputDir = document.querySelector("#dirCliente"),
     $inputTel = document.querySelector("#telCliente"),
-    $btnCliente = document.querySelector("#btnCliente");
+    $btnBusCliente = document.querySelector("#btnBusCliente"),
+    $btnRegCliente = document.querySelector("#btnRegCliente");
 
-
-$btnCliente.addEventListener("click", (e)=>{
-    e.preventDefault();
+$inputRif.addEventListener("submit", (e)=>{
     buscarCliente()
+})
+$btnBusCliente.addEventListener("click", (e)=>{
+    e.preventDefault();
+    buscarCliente();
 });
+$btnRegCliente.addEventListener("click", (e)=>{
+    e.preventDefault();
+    registrarCliente();
+})
 
 let $inputNomArt = document.querySelector("#nomArt"),
     $inputCod = document.querySelector("#codArt"),
